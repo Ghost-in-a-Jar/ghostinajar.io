65ded9f0c75716f2dcb9415cf3e6475c2a952a5d
Ghost-in-a-Jar <tristan.garwood@gmail.com>
2016-07-12 21:54:11 -0400

    testing clean blog template index.html

--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,9 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, "index.html", {})
+def posts(request):
+    return render(request, "post.html", {})
+def about(request):
+    return render(request, "about.html", {})
+def contact(request):
+    return render(request, "contact.html", {})
