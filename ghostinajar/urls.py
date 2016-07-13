67dcc460261aec19cf5f30ef6e594abbcce17461
Ghost-in-a-Jar <tristan.garwood@gmail.com>
2016-07-12 21:31:21 -0400

    testing clean blog template index.html

--- ghostinajar/urls.py
+++ ghostinajar/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 from django.contrib import admin
+from django.conf.urls.static import static
 from blog import views
 
 urlpatterns = [
     url(r'^$', views.index),
     #url(r'^$', TemplateView.as_view(template_name='index.html')),
     url(r'^admin/', include(admin.site.urls)),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
