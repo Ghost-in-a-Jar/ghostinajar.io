a8da3a5a29911de5ececc998f1eef47f4c08d1ce
Ghost-in-a-Jar <tristan.garwood@gmail.com>
2016-07-12 20:13:16 -0400

    adding blog model to admin.py

--- ghostinajar/urls.py
+++ ghostinajar/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.views.generic.base import TemplateView
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = url('',
     url(r'^$', TemplateView.as_view(template_name='base.html')),
     url(r'^admin/', include(admin.site.urls)),
 )
