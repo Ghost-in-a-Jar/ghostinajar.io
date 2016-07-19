7abe83371c643524246a9968246dcbfe280d1349
Ghost-in-a-Jar <tristan.garwood@gmail.com>
2016-07-19 19:27:28 -0400

    testing clean blog template index.html

--- blog/views.py
+++ blog/views.py
@@ -5,19 +5,18 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 def index(request):
-    def index(request):
-        # Obtain the context from the HTTP request.
-        context = RequestContext(request)
+    # Obtain the context from the HTTP request.
+    context = RequestContext(request)
 
-        # Query the database for a list of ALL categories currently stored.
-        # Order the categories by no. likes in descending order.
-        # Retrieve the top 5 only - or all if less than 5.
-        # Place the list in our context_dict dictionary which will be passed to the template engine.
-        post_list = Post.objects.order_by('updated')[:5]
-        context_dict = {'posts': post_list}
+    # Query the database for a list of ALL categories currently stored.
+    # Order the categories by no. likes in descending order.
+    # Retrieve the top 5 only - or all if less than 5.
+    # Place the list in our context_dict dictionary which will be passed to the template engine.
+    post_list = Post.objects.order_by('updated')[:5]
+    context_dict = {'posts': post_list}
+    # Render the response and send it back!
+    return render_to_response('static/index.html', context_dict, context)
 
-        # Render the response and send it back!
-        return render_to_response('static/index.html', context_dict, context)
 def posts(request):
     return render(request, "post.html", {})
 def about(request):
