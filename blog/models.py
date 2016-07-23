fd0904f4f333ed3415fa99677c47a383a444f93e
Ghost-in-a-Jar <tristan.garwood@gmail.com>
2016-07-23 17:09:27 -0400

    implementing endless pagination, template extending

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now = False, auto_now_add=True)
     updated = models.DateTimeField(auto_now = True, auto_now_add=False)
-    content_preview=' '.join(content.value_to_string().split(' ')[:50])
+    content_preview=' '.join(content.value_to_string(models.TextField).split(' ')[:50])
 
 
 
