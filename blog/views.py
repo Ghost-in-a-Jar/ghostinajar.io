1b112a099a4401be275c68b00ae35e40b576d484
Ghost-in-a-Jar <tristan.garwood@gmail.com>
2016-07-23 17:58:33 -0400

    implementing endless pagination, template extending

--- blog/views.py
+++ blog/views.py
@@ -13,7 +13,7 @@ def home(request, template='home.html'):
     # Order the categories by no. likes in descending order.
     # Retrieve the top 5 only - or all if less than 5.
     # Place the list in our context_dict dictionary which will be passed to the template engine.
-    post_list = Post.objects.order_by('updated')[:5]
+    post_list = Post.objects.order_by('updated')[:3]
     context_dict = {'post_list': post_list}
     # Render the response and send it back!
     return render_to_response(template, context_dict, context)
