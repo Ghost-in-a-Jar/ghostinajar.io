bf0d2041edf0030a67ae5867dfb5d82a9e511e57
Ghost-in-a-Jar <tristan.garwood@gmail.com>
2016-07-23 21:48:25 -0400

    implementing endless pagination, template extending

--- blog/forms.py
+++ blog/forms.py
@@ -2,9 +2,9 @@ from django import forms
 
 # our new form
 class ContactForm(forms.Form):
-    contact_name = forms.CharField(required=True, widget=forms.TextInput(attrs={'class':'form-control input-md'}))
-    contact_email = forms.EmailField(required=True, widget=forms.TextInput(attrs={'class':'form-control input-md'}))
+    contact_name = forms.CharField(required=True, widget=forms.TextInput(attrs={'class':'form-control input-md', 'placeholder':'Name'}))
+    contact_email = forms.EmailField(required=True, widget=forms.TextInput(attrs={'class':'form-control input-md', 'placeholder':'Email'}))
     content = forms.CharField(
         required=True,
-        widget=forms.Textarea(attrs={'class':'form-control input-md'})
+        widget=forms.Textarea(attrs={'class':'form-control input-md', 'placeholder':'Message'})
     )
