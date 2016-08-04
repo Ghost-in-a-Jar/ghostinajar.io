d66427c3e364773d1b664ec02ce6004ccead08eb
Ghost-in-a-Jar <tristan.garwood@gmail.com>
2016-08-04 12:34:20 -0400

    changing post listing by date to desc order

--- blog/views.py
+++ blog/views.py
@@ -19,14 +19,14 @@ def home(request, template='home.html'):
     # Order the categories by no. likes in descending order.
     # Retrieve the top 5 only - or all if less than 5.
     # Place the list in our context_dict dictionary which will be passed to the template engine.
-    post_list = Post.objects.order_by('updated')[:3]
+    post_list = Post.objects.order_by('-updated')[:3]
     context_dict = {'post_list': post_list}
     # Render the response and send it back!
     return render_to_response(template, context_dict, context)
 
 @page_template('post_index_page.html')
 def posts(request, template='post_index.html', extra_context=None):
-    post_list=Post.objects.all()
+    post_list=Post.objects.order_by('-updated')
     context = {
         'post_list':post_list,
     }
